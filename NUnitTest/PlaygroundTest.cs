6ab023c82ec145f37f5dc0a99e8d77667831a6ca
Ваня Задорожній <vanyazadorozhnii99493@gmail.com>
2023-12-20 17:12:32 +0200

    Add playground test

---
+++ NUnitTest/PlaygroundTest.cs
@@ -0,0 +1,55 @@
+﻿using ClassLibrary;
+
+namespace NUnitTest
+{
+    public class PlaygroundTest
+    {
+        Playground _playground;
+
+        [SetUp]
+        public void Setup()
+        {
+            _playground = new Playground(new char[,] {
+                { '#', '#', '#', '#', '#', '#', '#' },
+                { '#', ' ', ' ', ' ', ' ', ' ', '#' },
+                { '#', ' ', '#', ' ', '#', ' ', '#' },
+                { '#', ' ', ' ', ' ', ' ', ' ', '#' },
+                { '#', ' ', '#', ' ', '#', ' ', '#' },
+                { '#', ' ', ' ', ' ', ' ', ' ', '#' },
+                { '#', '#', '#', '#', '#', '#', '#' }
+            });
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Player player = new Player('@', 3, 3);
+
+            Assert.AreEqual(_playground.SpawnPlayer(player), true);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Player player = new Player('@', 2, 2);
+
+            Assert.AreEqual(_playground.SpawnPlayer(player), false);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Player player = new Player('@', 0, 0);
+
+            Assert.AreEqual(_playground.SpawnPlayer(player), false);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Player player = new Player('@', 4, 3);
+
+            Assert.AreEqual(_playground.SpawnPlayer(player), true);
+        }
+    }
+}
